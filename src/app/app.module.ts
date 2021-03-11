5a8b928997b6669320af223d392d48ed82ed83d9
maherynyainajacob16 <maherynyainajacob16@gmail.com>
2021-03-11 11:24:17 +0300

    Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { ProduitComponent } from './produit/produit.component';
+import { CommandeComponent } from './commande/commande.component';
+import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ProduitComponent,
+    CommandeComponent,
+    OtherComponent
   ],
   imports: [
     BrowserModule,
